57e9a2e49c36c8462aac7e7601be4cb141e966da
Trevor Gicheru <112418001+tgicherusmu@users.noreply.github.com>
2023-05-18 04:05:08 -0500

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,28 +1,30 @@
 #define CATCH_CONFIG_RUNNER
 #include "Functions.h"
 
-int main(int argc, char *argv[])
+int main(int argc, char* argv[])
 {
 
-    // get tweets
-    cout << "Reading training file..." << endl;
-    readTrainFile("../input/train_dataset_20k.csv");
-    // get words
-    addWords(positiveTweets, posWords);
-    addWords(negativeTweets, negativeWords);
+        // get tweets
+        cout<<"Reading training file..."<<endl;
+        readTrainFile("../input/train_dataset_20k.csv");
+        // get words
+        addWords(positiveTweets, positiveWords);
+        addWords(negativeTweets, negativeWords);
 
-    cout << "Reading test file..." << endl;
-    // add test tweets and predicted sentiment to vector of type Tweets
-    readTestFile("../input/test_dataset_10k.csv");
+        cout<<"Reading test file..."<<endl;
+        // add test tweets and predicted sentiment to vector of type Tweets
+        readTestFile("../input/test_dataset_10k.csv");
 
-    cout << "Calculating results..." << endl;
-    // read in actual sentiment and id's and put in another Tweet vector
-    getActualSentiment("../input/test_dataset_sentiment_10k.csv");
+        cout<<"Calculating results..."<<endl;
+        // read in actual sentiment and id's and put in another Tweet vector
+        getActualSentiment("../input/test_dataset_sentiment_10k.csv");
 
-    // compare the results of the two Tweet vectors
-    printResults(argv[1]);
+        // compare the results of the two Tweet vectors
+        printResults(argv[1]);
 
-    cout << "Complete!" << endl;
+
+        cout << "Complete!" << endl;
+
 
     return 0;
 }
