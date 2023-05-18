b117d048971635c0130883d70c1e25a71cbab1be
Trevor Gicheru <112418001+tgicherusmu@users.noreply.github.com>
2023-05-18 04:21:21 -0500

    Update Functions.cpp

--- src/Functions.cpp
+++ src/Functions.cpp
@@ -1,96 +1,50 @@
-#include "DSString.h"
+#ifndef PA01_SENTIMENT_FUNCTIONS_H
+#define PA01_SENTIMENT_FUNCTIONS_H
 
-// default constructor
-DSString::DSString() {
-    str = NULL;
-}
-// constructor
-DSString::DSString(const char* s) {
-    str = new char[strlen(s) + 1];
-    strcpy(str, s);
-}
-//  copy constructor
-DSString::DSString(const DSString& s) {
-    str = new char[strlen(s.str) + 1];
-    strcpy(str, s.str);
-}
-// destructor
-DSString::~DSString() {
-    delete[]str;
-    str = NULL;
-}
-// assignment operator
-DSString& DSString::operator= (const char* s) {
-    str = new char[strlen(s) + 1];
-    strcpy(str, s);
-    return *this;
-}
-// assignment operator
-DSString& DSString::operator= (const DSString& s) {
-    str = new char[strlen(s.str) + 1];
-    strcpy(str, s.str);
-    return *this;
-}
-// comparison operator
-bool DSString::operator< (const DSString& s) const{
-    return strcmp(str, s.str) < 0;
-}
-// comparison operator
-bool DSString::operator< (const char* s) const{
-    return strcmp(str, s) < 0;
-}
-// comparison operator
-bool DSString::operator> (const DSString& s) const{
-    return strcmp(str, s.str) > 0;
-}
-// comparison operator
-bool DSString::operator> (const char* s) const{
-    return strcmp(str, s) > 0;
-}
-// comparison operator
-bool DSString::operator== (const char* s) const {
-    return strcmp(str, s) == 0;
-}
-// comparison operator
-bool DSString::operator== (const DSString& s) const{
-    return strcmp(str, s.str) == 0;
-}
-DSString DSString::operator+ (const DSString& s) {
-    DSString temp;
-    temp.str = new char[strlen(str) + strlen(s.str) + 1]; // allocates memory for the concatenated 
-    strcpy(temp.str, str); // copies the contents of the first string (str) into temp.
-    strcat(temp.str, s.str); //concatenates the second string (s.str) onto temp.
-    return temp;
-}
-//returns a reference to the character at the specified index in the string. If the index is out of bounds, the operator returns the first character of the string
-char& DSString::operator[] (const int i) {
-    // return first char if index is out of bounds
-    if (i > strlen(str))
-        return str[0]; //returns the first character of the string if the index is out of bounds.
-    return str[i]; //returns the character at the specified index in the string
-}
-int DSString::getLength() {
-    return strlen(str);
-}
-DSString DSString::substring(int start, int numChars) {
-    DSString temp;
-    // return object invoker if index is out of bounds
-    if (start > strlen(str) + 1)
-        return *this;
 
-    temp.str = new char[numChars + 1];
+#include<iostream>
+#include<fstream>
+#include<sstream>
+#include<vector>
+#include<algorithm>
+#include<iomanip>
+#include"Word.h"
+#include "Tweets.h"
+#include"DSString.h"
+using namespace std;
 
-    for (int i = 0; i < numChars; i++)
-        temp.str[i] = str[start + i];
+// vector of positive and negative tweets after reading training file
 
-    temp.str[numChars] = '\0';
-    return temp;
-}
-char* DSString::c_str() {
-    char* s = str;
-    return s;
-}
-std::ostream& operator<< (std::ostream& os, const DSString& s) {
-    os << s.str;
-    return os;
-}
+vector<DSString> createVector();
+extern vector<DSString>positiveTweets;
+extern vector<DSString>negativeTweets;
+//put tweets into positive and negative vector based on their sentiment
+void readTrainFile(char[]);
+void addTweet(DSString tweet, DSString sentiment);
+
+// list of words to be filtered out of the tweets
+extern vector<DSString>nonsentimentalWords;
+// add words to positive and negative words list along with their occurrence
+void addWords(vector<DSString>&, vector<Word>&);
+void chopUpWord(DSString* word);
+bool validWord(DSString word);
+void removeChar(DSString* word, char c);
+// words split into positive and negative from corresponding tweet list
+extern vector<Word>positiveWords;
+extern vector<Word>negativeWords;
+
+// predict sentiment of tweet, either 4 for positive or 0 for negative
+void readTestFile(char[]);
+int predictTweet(char tweet[]);
+void wordExists(int& sentimentScore, DSString& word);
+// list of tweets after classifying sentiment
+extern vector<Tweets>tweetSentimentResults;
+
+// get actual sentiment and store in vector with id
+void getActualSentiment(char[]);
+extern vector<Tweets>tweetActualSentimentResults;
+
+// compare results and output
+void printResults(char[]);
+
+#endif //PA01_SENTIMENT_FUNCTIONS_H
