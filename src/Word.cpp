8ff09401dd5d5bcd7974dd27677b5d3fb82baa8a
Trevor Gicheru <112418001+tgicherusmu@users.noreply.github.com>
2023-05-18 04:05:45 -0500

    Update Word.cpp

--- src/Word.cpp
+++ src/Word.cpp
@@ -1,8 +1,7 @@
 #include "Word.h"
 
 Word::Word(const char*x) {
-    word = x; 
-    quantity = 1;
+    word = x; quantity = 1;
 }
 bool Word::operator==(const Word& obj2) const {
     if (this->word == obj2.word)
