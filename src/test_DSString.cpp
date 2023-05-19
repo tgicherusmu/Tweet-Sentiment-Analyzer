d8487983d3afde1df6358076cccf0b9e01404f31
Trevor Gicheru <112418001+tgicherusmu@users.noreply.github.com>
2023-05-18 22:18:41 -0500

    Update test_DSString.cpp

--- src/test_DSString.cpp
+++ src/test_DSString.cpp
@@ -1,54 +1,54 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
+// #include <iostream>
+// #include <vector>
+// #include <algorithm>
 
-#include "DSString.h"
+// #include "DSString.h"
 
-using namespace std;
+// using namespace std;
 
-int main()
-{  
-    DSString myString = "Hello, World!";
-    cout << myString << endl;
+// int main()
+// {  
+//     DSString myString = "Hello, World!";
+//     cout << myString << endl;
 
-    // this uses the one argument constructor in DSString and then the inherited operator=
-    myString = "Good bye!";
-    cout << myString << endl;
+//     // this uses the one argument constructor in DSString and then the inherited operator=
+//     myString = "Good bye!";
+//     cout << myString << endl;
 
-    cout << "substr: " << myString.substring(5, 3) << endl;
+//     cout << "substr: " << myString.substring(5, 3) << endl;
 
-    DSString a = "test";
-    DSString b = a;
+//     DSString a = "test";
+//     DSString b = a;
 
-    cout << boolalpha;
-    cout << (a == b) << endl;
+//     cout << boolalpha;
+//     cout << (a == b) << endl;
 
-    // use initialization list (see DSVector.h)
-    vector<DSString> strings = {
-        DSString("bbb"),
-        DSString("aaa"),
-        DSString("ddd"),
-        DSString("eee"),
-        DSString("ccc")};
+//     // use initialization list (see DSVector.h)
+//     vector<DSString> strings = {
+//         DSString("bbb"),
+//         DSString("aaa"),
+//         DSString("ddd"),
+//         DSString("eee"),
+//         DSString("ccc")};
 
-    // find strings
-    for (const auto &s : strings)
-        cout
-            << s << "\n";
+//     // find strings
+//     for (const auto &s : strings)
+//         cout
+//             << s << "\n";
 
-    cout << "found ddd: " << (find(strings.begin(), strings.end(), DSString("ddd")) != strings.end()) << endl;
-    cout << "found zzz: " << (find(strings.begin(), strings.end(), DSString("zzz")) != strings.end()) << endl;
+//     cout << "found ddd: " << (find(strings.begin(), strings.end(), DSString("ddd")) != strings.end()) << endl;
+//     cout << "found zzz: " << (find(strings.begin(), strings.end(), DSString("zzz")) != strings.end()) << endl;
 
-    // sorting
-    sort(strings.begin(), strings.end());
+//     // sorting
+//     sort(strings.begin(), strings.end());
 
-    for (const auto &s : strings)
-        cout
-            << s << "\n";
+//     for (const auto &s : strings)
+//         cout
+//             << s << "\n";
 
-    // now we can do more efficient search
-    cout << "found ddd: " << binary_search(strings.begin(), strings.end(), DSString("ddd")) << endl;
-    cout << "found zzz: " << binary_search(strings.begin(), strings.end(), DSString("zzz")) << endl;
+//     // now we can do more efficient search
+//     cout << "found ddd: " << binary_search(strings.begin(), strings.end(), DSString("ddd")) << endl;
+//     cout << "found zzz: " << binary_search(strings.begin(), strings.end(), DSString("zzz")) << endl;
 
-    return 0;
-}
+//     return 0;
+// }
